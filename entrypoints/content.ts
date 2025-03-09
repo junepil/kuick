188ec9975af698030c6c8e134f7d3061136aa813
Junepil Lee <hanu9257@gmail.com>
2025-03-10 01:42:58 +0900

    feat: :sparkles: Content script 구현

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,20 @@
+import { Group } from "@/components/GroupContainer";
+import useRegister from "@/hooks/register";
+
 export default defineContentScript({
-  matches: ["<all_urls>"],
+  matches: [
+    "<all_urls>",
+    "https://wein.konkuk.ac.kr/ptfol/cmnt/cube/findCubeResveStep2.do",
+    "https://wein.konkuk.ac.kr/ptfol/cmnt/hub/findCubeResveStep2.do",
+  ],
   main() {
-    console.log("hello world");
+    console.log("content script");
+
+    browser.runtime.onMessage.addListener((message) => {
+      console.log("message recieved: ", message.content);
+      const group: Group = message.content;
+
+      useRegister(group);
+    });
   },
 });
