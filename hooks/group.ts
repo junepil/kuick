b75232c99974bbd5ee891d850fbb8d99be90bfb2
Junepil Lee <hanu9257@gmail.com>
2025-03-10 19:27:33 +0900

    chore: :fire: group 훅 삭제
    사용처가 사라졌음

--- hooks/group.ts
+++
@@ -1,9 +0,0 @@
-const persistGroups = async (groups: Group[]) => {
-  await browser.storage.local.set({ groups });
-};
-
-const loadGroups = async () => {
-  return (await browser.storage.local.get()) as Group[];
-};
-
-export { persistGroups, loadGroups };
