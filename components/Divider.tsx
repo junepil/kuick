fcb8dad8f10602994e49408f59989a1e9c6ee1af
Junepil Lee <hanu9257@gmail.com>
2025-03-10 21:05:21 +0900

    refactor: :sparkles: Divider 디자인 수정

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,6 +1,6 @@
 const Divider = () => {
   return (
-    <div className='h-0 w-full p-2 border-0 border-t-2 border-gray-300 col-span-2'></div>
+    <div className='h-0 w-full border-0 border-t-2 border-gray-300 col-span-2'></div>
   );
 };
 
