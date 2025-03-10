cdf5517878a94f9bfe8277b4eb0cc13ba7eb24e7
Junepil Lee <hanu9257@gmail.com>
2025-03-10 21:55:16 +0900

    test: :sparkles: Input 최대 크기 설정

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,7 +7,7 @@ export default function Input({
   return (
     <input
       type='text'
-      className='h-8 rounded-md border-2 border-gray-300 p-2 outline-green-800'
+      className='max-w-28 h-8 rounded-md border-2 border-gray-300 p-2 outline-green-800 w-auto'
       placeholder={placeHolder}
       {...props}
     />
