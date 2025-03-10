1b0c663ad9e02cc756b7233748627ed69bb78ea8
Junepil Lee <hanu9257@gmail.com>
2025-03-10 09:36:21 +0900

    refactor: :fire: 백그라운드 스크립트 핸들러 수정

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,46 +1,11 @@
-import useApply from "@/hooks/apply";
-import { setGroup, getGroup } from "@/hooks/group";
-
 export default defineBackground(() => {
   console.log("Hello background!", { id: browser.runtime.id });
 
-  // browser.storage.sync.set({ key: "value" });
-  // useMessageListener();
+  browser.runtime.onConnect.addListener((port) => {
+    console.log("Popup opened");
+
+    port.onDisconnect.addListener(() => {
+      console.log("Popup closed");
+    });
+  });
 });
-
-const useMessageListener = () => {
-  browser.runtime.onMessage.addListener(
-    async (message, sender, sendResponse) => {
-      const tabs = await browser.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-
-      const tabId = tabs[0].id;
-
-      const group = {
-        name: "test group",
-        members: ["202111345", "202111345"],
-      };
-
-      if (!tabId) return;
-
-      switch (message.action) {
-        case "set":
-          await setGroup(group);
-          break;
-        case "get":
-          console.log(await getGroup(group.name));
-          break;
-        case "apply":
-          browser.scripting.executeScript({
-            target: { tabId: tabId },
-            func: useApply,
-          });
-          break;
-        default:
-          console.log("default");
-      }
-    },
-  );
-};
