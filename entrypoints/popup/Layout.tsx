3f4e154a7b33114f2f06a403690e2e20f0f4fe91
Junepil Lee <hanu9257@gmail.com>
2025-03-10 21:54:42 +0900

    refactor: :art: Layout 수정

--- entrypoints/popup/Layout.tsx
+++ entrypoints/popup/Layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <div className='w-md h-fit grid grid-cols-2 gap-2 p-2'>
+      <div className='w-md h-fit flex flex-col gap-2'>
         <h2 className='p-4 text-5xl font-black text-green-800 col-span-2 text-center'>
           KUICK
         </h2>
