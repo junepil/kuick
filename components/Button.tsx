a0f13196ad1d817116f99f89e0c679638dc5ee74
Junepil Lee <hanu9257@gmail.com>
2025-03-11 22:51:20 +0900

    feat: :sparkles: Button에 cva 라이브러리 적용

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,42 @@
-import { ReactNode } from "react";
+import { cn } from "@/lib/util";
+import { cva, VariantProps } from "class-variance-authority";
+import { ButtonHTMLAttributes } from "react";
 
-type ButtonProps = {
-  onClick?: () => void;
-  children: ReactNode;
-};
+const buttonVariants = cva(
+  "flex items-center font-[Inter] justify-center text-md text-gray-100 rounded-xl bg-gradient-to-bl ease-in-out w-fit",
+  {
+    variants: {
+      variant: {
+        default: "from-cyan-800 to-emerald-800 hover:backdrop-blur-2xl",
+        destructive: "from-orange-800 to-pink-700",
+        transparent:
+          "border-2 border-neutral-50 backdrop-blur-2xl text-neutral-600 font-bold hover:border-cyan-800",
+      },
+      size: {
+        md: "h-10 px-4 py-5",
+        lg: "h-14 px-4 py-8",
+        sm: "h-8 px-2 py-4",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "md",
+    },
+  },
+);
 
-const Button = ({ onClick, children, ...props }: ButtonProps) => {
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = ({ className, size, variant, ...props }: ButtonProps) => {
   return (
     <button
       type='button'
-      onClick={onClick}
-      className='flex items-center justify-center h-8 p-2.5 text-md text-gray-100 rounded-lg bg-gradient-to-bl from-cyan-800 to-emerald-800 hover:from-50% ease-in-out'
+      className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    >
-      {children ?? "버튼"}
-    </button>
+    />
   );
 };
 
-export { Button };
+export { Button, buttonVariants };
