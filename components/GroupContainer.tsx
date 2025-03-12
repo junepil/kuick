440bbc79e3373159f777e6bc974d48b0ac0128fa
Junepil Lee <hanu9257@gmail.com>
2025-03-12 17:10:07 +0900

    refactor: :art: GroupContainer 구조 단순화

--- components/GroupContainer.tsx
+++ components/GroupContainer.tsx
@@ -19,13 +19,14 @@ type GroupContainerProps = {
 const GroupContainer = ({ group, onClick, onClose }: GroupContainerProps) => {
   return (
     <Box onClose={onClose} title={group.name}>
-      <div className='w-full flex flex-col gap-2'>
-        <ul className='flex flex-col h-full p-2 gap-2 items-start'>
+      <div className='w-full flex flex-col gap-2 items-end'>
+        <ul className='flex flex-col h-full w-full p-2 gap-2 items-start'>
           {group.members.map((member, index) => (
-            <li className='w-fit flex gap-2' key={index}>
-              <div className='text-sm font-[Inter] font-semibold text-gray-800'>
-                {member}
-              </div>
+            <li
+              className='w-fit flex gap-2 text-sm font-[Inter] font-semibold text-stone-800'
+              key={index}
+            >
+              {member}
             </li>
           ))}
         </ul>
