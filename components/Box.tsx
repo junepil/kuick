e9a5f9448b6e764f9268d080539ce9f587368862
Junepil Lee <hanu9257@gmail.com>
2025-03-17 23:15:44 +0900

    refactor: :lipstick: Box의 아이콘 호버 디자인 수정

--- components/Box.tsx
+++ components/Box.tsx
@@ -10,7 +10,7 @@ interface BoxProps {
 
 const Box = ({ onClose, children, icon, title }: BoxProps) => {
   return (
-    <div className='p-6 pt-10 border-2 border-stone-300 bg-stone-50 rounded-2xl relative hover:border-cyan-700 ease-in-out duration-300'>
+    <div className='p-6 pt-12 bg-stone-50 rounded-2xl relative'>
       {title && (
         <div className='p-1.5 font-[Inter] text-sm font-semibold absolute start-2 top-2 text-stone-500 max-w-[75%] overflow-hidden'>
           {title}
@@ -21,7 +21,7 @@ const Box = ({ onClose, children, icon, title }: BoxProps) => {
       ) : (
         <IoClose
           onClick={onClose}
-          className='ease-in-out duration-300 absolute end-2 top-2 text-3xl text-stone-400 hover:text-emerald-800 hover:bg-gradient-to-bl from-40% from-stone-100 to-stone-300 bg-stone-50 rounded-full p-1.5'
+          className='ease-in-out duration-300 absolute end-2 top-2 text-3xl text-stone-400 hover:text-emerald-800 hover:bg-stone-200 rounded-full p-1.5'
         />
       )}
       {children}
