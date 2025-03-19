18c63d957ea9daa4c21525a6e8818c87f7ca6a39
Junepil Lee <hanu9257@gmail.com>
2025-03-19 14:03:18 +0900

    feat: :sparkles: Box 컴포넌트 prop 및 디자인 변경

--- components/Box.tsx
+++ components/Box.tsx
@@ -6,13 +6,17 @@ interface BoxProps {
   children?: ReactNode;
   icon?: ReactNode;
   title?: string;
+  ref: React.Ref<any>;
 }
 
-const Box = ({ onClose, children, icon, title }: BoxProps) => {
+const Box = ({ onClose, children, icon, title, ref }: BoxProps) => {
   return (
-    <div className='p-6 pt-12 bg-stone-50 rounded-2xl relative'>
+    <div
+      className='p-6 pt-12 bg-stone-50 rounded-2xl relative snap-center'
+      ref={ref}
+    >
       {title && (
-        <div className='p-1.5 font-[Inter] text-sm font-semibold absolute start-2 top-2 text-stone-500 max-w-[75%] overflow-hidden'>
+        <div className='p-1.5 font-[Inter] text-sm font-semibold absolute start-4 top-4 text-stone-500 max-w-[75%] overflow-hidden hover:cursor-default'>
           {title}
         </div>
       )}
@@ -21,7 +25,7 @@ const Box = ({ onClose, children, icon, title }: BoxProps) => {
       ) : (
         <IoClose
           onClick={onClose}
-          className='ease-in-out duration-300 absolute end-2 top-2 text-3xl text-stone-400 hover:text-emerald-800 hover:bg-stone-200 rounded-full p-1.5'
+          className='ease-in-out translate-x-[50%] translate-y-[-50%] duration-300 absolute end-8 top-8 text-3xl bg-stone-100 text-stone-400 hover:cursor-pointer rounded-full p-1.5'
         />
       )}
       {children}
