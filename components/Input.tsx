7ecccef3d700b214a83f8a0663f33b77fac8ab5d
Junepil Lee <hanu9257@gmail.com>
2025-03-19 14:22:12 +0900

    feat: :sparkles: Input 프롭과 디자인 변경

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,7 +3,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import { InputHTMLAttributes } from "react";
 
 const inputVarriants = cva(
-  "font-[Inter] w-full font-semibold h-8 rounded-xl border-2 border-stone-300 p-3 outline-cyan-700 ease-in-out duration-300",
+  "font-[Inter] font-semibold h-8 rounded-lg border-2 border-stone-300 p-3 px-2 outline-indigo-700 ease-in-out duration-100",
   {
     variants: {},
     defaultVariants: {},
@@ -12,11 +12,27 @@ const inputVarriants = cva(
 
 interface InputProps
   extends InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof inputVarriants> {}
+    VariantProps<typeof inputVarriants> {
+  invalid?: boolean;
+  message?: string;
+}
 
-const Input = ({ className, ...props }: InputProps) => {
+const Input = ({ className, invalid, message, ...props }: InputProps) => {
   return (
-    <input type='text' className={cn(inputVarriants(className))} {...props} />
+    <div className='relative w-fit h-fit'>
+      <input
+        type='text'
+        className={cn(inputVarriants(className), {
+          "outline-red-700": invalid,
+        })}
+        {...props}
+      />
+      {invalid && (
+        <p className='px-2 absolute font-[Inter] text-[10px] text-red-700'>
+          {message}
+        </p>
+      )}
+    </div>
   );
 };
 
