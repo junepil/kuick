42ad6d1c732862bda53f34ab2e767abc623a6623
Junepil Lee <hanu9257@gmail.com>
2025-03-19 17:03:56 +0900

    feat: :fire: Content script의 필요 없는 권한 및 로그 삭제

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -3,10 +3,8 @@ import useRegister from "@/hooks/register";
 import { targetUrl } from "@/model/url";
 
 export default defineContentScript({
-  matches: ["<all_urls>", ...targetUrl],
+  matches: targetUrl,
   main() {
-    console.log("content script");
-
     browser.runtime.onMessage.addListener((message) => {
       console.log("message recieved: ", message.content);
       const group: Group = message.content;
