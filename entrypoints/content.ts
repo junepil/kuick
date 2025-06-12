3101b263f2503b979988576813f944535959425f
Junepil Lee <hanu9257@gmail.com>
2025-06-12 15:55:04 +0900

    chore: :mute: Content script 로그 삭제

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -5,9 +5,7 @@ import { targetUrl } from "@/model/url";
 export default defineContentScript({
   matches: targetUrl,
   main() {
-    
     browser.runtime.onMessage.addListener((message) => {
-      console.log("message recieved: ", message.content);
       const group: Group = message.content;
 
       useRegister(group);
